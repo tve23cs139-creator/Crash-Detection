e2c478e1410dc742560a5f5d5e6a0283f7122e46
Navaneeth832 <tve23cs099@cet.ac.in>
2026-03-24 17:25:44 +0530

    vite

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dashboard/',
   plugins: [react()],
+  base: '/Crash-Detection/dashboard/', // <--- THIS IS THE KEY
 })
